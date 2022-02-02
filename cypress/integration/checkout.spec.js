c52b6ad7a2f6e13ddd9caec747bea70166450d66
Joshua Martin <the.manuscriptmaster@gmail.com>
2022-02-02 11:55:20 -0500

    Add test case for multiple cart rows

--- cypress/integration/checkout.spec.js
+++ cypress/integration/checkout.spec.js
@@ -1,5 +1,14 @@
 /// <reference types="cypress" />
-import { AVOCADO_TOAST } from '../../items.js';
+import currency from 'currency.js';
+import { AVOCADO_TOAST, CHEESE_BURGER, EGGS_BENEDICT } from '../../items.js';
+
+// Utils
+export const add = (a, b) => currency(a).add(b).format();
+
+export const sum = (moneys) => moneys.reduce(add, '$0.00');
+
+export const multiply = (value, rate) =>
+  currency(value).multiply(rate).format();
 
 const addItemToCart = ({ name }) => {
   cy.get('app-menu')
@@ -41,8 +50,9 @@ const testQuantityIs = ({ name }, quantity) => {
     .shadow()
     .contains(name)
     .parent('article')
-    .get('select')
-    .should('have.value', quantity.toString());
+    .within(() => {
+      cy.get('select').should('have.value', quantity.toString());
+    });
 };
 
 const updateQuantity = ({ name }, quantity) => {
@@ -50,8 +60,9 @@ const updateQuantity = ({ name }, quantity) => {
     .shadow()
     .contains(name)
     .parent('article')
-    .get('select')
-    .select(quantity.toString());
+    .within(() => {
+      cy.get('select').select(quantity.toString());
+    });
 };
 
 const testOrderTotalIs = (money) => {
@@ -59,7 +70,7 @@ const testOrderTotalIs = (money) => {
 };
 
 describe('Checkout', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/');
   });
   it('displays an empty cart', () => {
@@ -80,6 +91,38 @@ describe('Checkout', () => {
   it('allows a user to update quantity on an item in the cart', () => {
     addItemToCart(AVOCADO_TOAST);
     testQuantityIs(AVOCADO_TOAST, 1);
+    testOrderTotalIs(AVOCADO_TOAST.price);
     updateQuantity(AVOCADO_TOAST, 2);
+    testOrderTotalIs(multiply(AVOCADO_TOAST.price, 2));
+  });
+  it('disallows a user from adding a redundant item to the cart', () => {
+    addItemToCart(AVOCADO_TOAST);
+    testCartCountIs(1);
+    testQuantityIs(AVOCADO_TOAST, 1);
+    cy.get('app-menu')
+      .shadow()
+      .contains(AVOCADO_TOAST.name)
+      .parent('article')
+      .contains('button', 'Buy')
+      .should('be.disabled');
+  });
+  it('allows a user to add multiple unique items to the cart', () => {
+    addItemToCart(AVOCADO_TOAST);
+    addItemToCart(CHEESE_BURGER);
+    addItemToCart(EGGS_BENEDICT);
+    testCartCountIs(3);
+    testOrderTotalIs(
+      sum([AVOCADO_TOAST.price, CHEESE_BURGER.price, EGGS_BENEDICT.price])
+    );
+    updateQuantity(CHEESE_BURGER, 2);
+    updateQuantity(EGGS_BENEDICT, 3);
+    testCartCountIs(6);
+    testOrderTotalIs(
+      sum([
+        AVOCADO_TOAST.price,
+        multiply(CHEESE_BURGER.price, 2),
+        multiply(EGGS_BENEDICT.price, 3),
+      ])
+    );
   });
 });
